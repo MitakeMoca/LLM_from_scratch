d366be41c61e50e9e4585b77f7406550eed06b69
MitakeMoca <1043530132@qq.com>
2025-10-28 23:44:21 +0800

    feat: GPT model

--- main_ch4.py
+++ main_ch4.py
@@ -1,15 +1,44 @@
 import tiktoken
 import torch
+from models.GPT_model import GPTModel
+from util.generate import generate_text_simple
 
 torch.manual_seed(123)
 
 tokenizer = tiktoken.get_encoding("gpt2")
-batch = []
-txt1 = "Every effort moves you"
-txt2 = "Every day holds a"
+txt = "Hello, I am"
 
-batch.append(torch.tensor(tokenizer.encode(txt1)))
-batch.append(torch.tensor(tokenizer.encode(txt2)))
+encoded = tokenizer.encode(txt)
+print(encoded)
 
-batch = torch.stack(batch, dim=0)
-print(batch)
+# 添加 batch 维度
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+print("encoded_tensor.shape: ", encoded_tensor.shape)
+
+# GPT2 模型配置
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,
+    "context_length": 1024,
+    "emb_dim": 768,
+    "n_heads": 12,
+    "n_layers": 12,
+    "drop_rate": 0.1,
+    "qkv_bias": False
+}
+
+model = GPTModel(GPT_CONFIG_124M)
+
+# 评估模式，将不会使用类似于 dropout 这种模块
+model.eval()
+out = generate_text_simple(
+    model=model,
+    idx=encoded_tensor,
+    max_new_tokens=6,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+
+print("Output: ", out)
+print("Output length: ", len(out[0]))
+
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print("decoded text: ", decoded_text)
